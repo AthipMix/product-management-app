244d3f0aa5cca64dd73492f4cc2018ed311bd9b7
AthipMix <674259028@webmail.npru.ac.th>
2026-09-23 16:56:47 +0700

    complete CRUD

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { connectDB } from "./db.js";
+import { connectDB } from "./config/database.js";
+import productRouter from "./router/productRouter.js";
 
 dotenv.config();
 const PORT = process.env.BACKEND_PORT;
@@ -10,6 +11,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/products", productRouter);
+
 connectDB();
 
 app.get("/", (req, res) => {
@@ -17,6 +20,7 @@ app.get("/", (req, res) => {
     .status(200)
     .send("<h> Welcome to Restful API Product Management App</h>");
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on: http://localhost:${PORT}`);
